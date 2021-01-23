749a4a08ebc41decacd748e0c14140e12e5c8251
CaptDread <domlycaon@gmail.com>
2021-01-22 20:56:31 -0500

    1stWave

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,34 @@
-<<<<<<< HEAD
-'use strict';
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-w = ctx.canvas.width = window.innerWidth;
-h = ctx.canvas.height = window.innerHeight;
-ctx.filter = "blur(3px)";
-nt = 0;
+// wave.js
 
-window.onresize = function () {
-  w = ctx.canvas.width = window.innerWidth;
-  h = ctx.canvas.height = window.innerHeight;
-  ctx.filter = "blur(3px)";
-};
+TweenLite.defaultEase = Sine.easeInOut;
+TweenLite.set("g", { y: window.innerHeight / 2 });
 
-function drawWave(n) {
-  nt += 0.013;
+var svg  = document.querySelectorAll("svg");
+var wave = document.querySelectorAll("#wave");
 
-  for (i = 0; i < n; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 6;
-    ctx.strokeStyle = "hsla(" + (150 + 20 * i) + ",100%,60%,1)";
-
-    for (x = 0; x < w; x++) {
-      var y = noise.simplex3(x / 400, 0.3 * i, nt) * 150;
-      ctx.lineTo(x, y + h / 2);
-    }
-
-    ctx.stroke();
-    ctx.closePath();
+for (let z = 0; z < svg.length; z ++){
+  var width = 1000;
+  var sinus = CustomEase.create("sinus", "M0,0 C0.4,0 0.3,1 0.5,1 0.7,1 0.6,0 1,0");
+  
+  var amplitude = 200;
+  var frequency = 20;
+  var segments  = 1300;
+  var interval  = width / segments;
+  
+  for (var i = 0; i <= segments; i++) {
+    
+    var norm = .8 - i / segments;
+    var point = wave[z].points.appendItem(svg[z].createSVGPoint());
+    
+    point.x = i * interval;
+    point.y = (amplitude / 1) * sinus(norm);
+      
+    TweenMax.to(point, 0.6, { y: -point.y, repeat: -1, yoyo: true }).progress(norm * frequency);  
   }
+
 }
 
-function render() {
-  ctx.fillStyle = "rgba(0,12,12,1)";
-  ctx.fillRect(0, 0, w, h);
-  drawWave(5);
-  requestAnimationFrame(render);
-}
-
-render();
-//# sourceMappingURL=main.js.map
 =======
 /**
  * This is an example of a basic node.js script that performs
